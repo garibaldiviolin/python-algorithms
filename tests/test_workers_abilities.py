2ee60c26b48c42ed669ac6a51ef46b96ec1d5300
neto <garibaldiviolin@gmail.com>
2021-04-05 22:59:49 -0300

    Replace assertEquals by assertEqual

--- tests/test_workers_abilities.py
+++ tests/test_workers_abilities.py
@@ -11,19 +11,19 @@ class CalculateMinimumDaysTestCase(TimedTestCase):
         return super().setUp()
 
     def test_calculate_with_small_input_minimum_days(self):
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days([1, 1, 2, 3, 5], 12),
             4
         )
 
     def test_calculate_with_small_input_exhausted(self):
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days([1, 1, 2, 3, 5], 17),
             9
         )
 
     def test_calculate_with_medium_input_minimum_days(self):
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days(
                 [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22],
                 244
@@ -33,14 +33,14 @@ class CalculateMinimumDaysTestCase(TimedTestCase):
 
     def test_calculate_with_big_input_minimum_days(self):
         big_list = [number for number in range(0, 30_000, 2)]
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days(big_list, 2_989_900),
             100
         )
 
     def test_calculate_with_big_input_exhausted(self):
         big_list = [number for number in range(0, 30_000, 2)]
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days(
                 big_list,
                 449_867_748
@@ -54,19 +54,19 @@ class CalculateMinimumDaysOptimizedTestCase(TimedTestCase):
         return super().setUp()
 
     def test_calculate_with_small_input_minimum_days(self):
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days_optimized([1, 1, 2, 3, 5], 12),
             4
         )
 
     def test_calculate_with_small_input_exhausted(self):
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days_optimized([1, 1, 2, 3, 5], 17),
             9
         )
 
     def test_calculate_with_medium_input_minimum_days(self):
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days_optimized(
                 [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22],
                 244
@@ -76,14 +76,14 @@ class CalculateMinimumDaysOptimizedTestCase(TimedTestCase):
 
     def test_calculate_with_big_input_minimum_days(self):
         big_list = [number for number in range(0, 30_000, 2)]
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days_optimized(big_list, 2_989_900),
             100
         )
 
     def test_calculate_with_big_input_exhausted(self):
         big_list = [number for number in range(0, 30_000, 2)]
-        self.assertEquals(
+        self.assertEqual(
             calculate_minimum_days_optimized(
                 big_list,
                 449_867_748
