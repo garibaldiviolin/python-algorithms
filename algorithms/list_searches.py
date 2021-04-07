69c5cce2615b4df961080dbea11641c88b47dc77
neto <garibaldiviolin@gmail.com>
2021-04-06 22:19:21 -0300

    Fix binary_search loop

--- algorithms/list_searches.py
+++ algorithms/list_searches.py
@@ -7,8 +7,8 @@ def binary_search(numbers_list, number):
     section_end = len(numbers_list) - 1
     section_middle = section_end // 2
 
-    while (number > numbers_list[section_start]
-            and number < numbers_list[section_end]):
+    while (number >= numbers_list[section_start]
+            and number <= numbers_list[section_end]):
         if number <= numbers_list[section_middle]:
             section_end = section_middle
             section_middle = section_middle // 2
